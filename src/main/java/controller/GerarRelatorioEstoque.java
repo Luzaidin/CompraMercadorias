e804f04b3866646976bd3d4978d21a6f13f3d68c
LuisBelo <luzaidinbelo@gmail.com>
2019-10-31 22:32:12 -0300

    jpa de gerar relatorio do estoque

--- src/main/java/controller/GerarRelatorioEstoque.java
+++ src/main/java/controller/GerarRelatorioEstoque.java
@@ -4,13 +4,14 @@ import model.Produto;
 import model.ProdutoDAO;
 import model.ProdutoDAOImpl;
 import java.util.List;
+import java.util.ArrayList;
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/relatorioestoque.action")
+@WebServlet(name = "relatorioestoque.action", urlPatterns = {"/relatorioestoque.action"})
 public class GerarRelatorioEstoque extends HttpServlet {   
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp){
@@ -20,21 +21,24 @@ public class GerarRelatorioEstoque extends HttpServlet {
         }catch(Exception e){} 
 
         ServletContext sc = req.getServletContext();
+        List<Produto> items = new ArrayList<>();
 
-        try{
-            ProdutoDAOImpl pd = new ProdutoDAOImpl();
-            List<Produto> items = pd.findAll();
-            req.setAttribute("gerarRelatorio", true);
-            req.setAttribute("quantidadeEstoque", items.size());
-        } catch(Exception e){
-            req.setAttribute("mensagem", "Erro ao gerar relatório do estoque.");
-            req.setAttribute("gerarRelatorio", false);
-        }
+         try{
+             ProdutoDAOImpl pd = new ProdutoDAOImpl();
+             items = pd.findAll();
+            
+             req.setAttribute("ListaProdutos", items);
+             req.setAttribute("gerarRelatorio", "Relatorio");
+             req.setAttribute("quantidadeEstoque", items.size());
+         } catch(Exception e){
+             req.setAttribute("mensagem", "Erro ao gerar relatório do estoque.");
+         }
 
         try {
             sc.getRequestDispatcher("/jsp/relatorioEstoque.jsp").forward(req, resp); 
         }catch(Exception e){
             //Tratamento de erro de IO ou de Servlet..
+            System.out.println(e);
         }        
     }
 }
